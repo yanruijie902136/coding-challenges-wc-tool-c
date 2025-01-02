8a999d666e33a6249ba6d2eee46447a10692996b
yanruijie902136 <yanruijie902136@gmail.com>
2025-01-03 04:43:22 +0800

    Complete steps 4, 5, and 6

--- src/ccwc.c
+++ src/ccwc.c
@@ -1,11 +1,12 @@
-#include <ctype.h>
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <stdbool.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wchar.h>
+#include <wctype.h>
 
 static int exit_status = EXIT_SUCCESS;
 
@@ -14,6 +15,7 @@ enum count_mode
         COUNT_BYTES = 0x01,
         COUNT_LINES = 0x02,
         COUNT_WORDS = 0x04,
+        COUNT_CHARS = 0x08,
 };
 
 struct input_args
@@ -24,7 +26,7 @@ struct input_args
 
 static void print_usage_and_exit(void)
 {
-        fprintf(stderr, "usage: ccwc [-clw] file ...\n");
+        fprintf(stderr, "usage: ccwc [-clmw] [file ...]\n");
         exit(EXIT_FAILURE);
 }
 
@@ -34,15 +36,20 @@ static struct input_args check_usage(int argc, char *const argv[])
         args.modes = 0;
 
         int opt;
-        while ( (opt = getopt(argc, argv, ":clw")) != -1)
+        while ( (opt = getopt(argc, argv, ":clmw")) != -1)
         {
                 switch (opt)
                 {
                 case 'c':
+                        args.modes &= ~COUNT_CHARS;
                         args.modes |= COUNT_BYTES;
                         break;
                 case 'l':
                         args.modes |= COUNT_LINES;
+                        break;
+                case 'm':
+                        args.modes &= ~COUNT_BYTES;
+                        args.modes |= COUNT_CHARS;
                         break;
                 case 'w':
                         args.modes |= COUNT_WORDS;
@@ -54,59 +61,45 @@ static struct input_args check_usage(int argc, char *const argv[])
         }
 
         args.files = argv + optind;
-        if (args.files[0] == NULL)
-        {
-                print_usage_and_exit();
-        }
         if (args.modes == 0)
         {
                 args.modes = COUNT_BYTES | COUNT_LINES | COUNT_WORDS;
         }
-
         return args;
 }
 
 static void report_file(const char *file, int modes)
 {
-        int fd = open(file, O_RDONLY);
-        if (fd < 0)
+        FILE *fp = stdin;
+        if (file != NULL && (fp = fopen(file, "r")) == NULL)
         {
                 warn("%s", file);
                 exit_status = EXIT_FAILURE;
                 return;
         }
 
-        static char buffer[BUFSIZ];
-        char prevch = ' ';
-        size_t nbytes = 0, nlines = 0, nwords = 0;
-        for ( ; ; )
-        {
-                ssize_t nr = read(fd, buffer, sizeof(buffer));
-                if (nr < 0)
-                {
-                        warn("%s", file);
-                        exit_status = EXIT_FAILURE;
-                        close(fd);
-                        return;
-                }
-                else if (nr == 0)
-                {
-                        break;
-                }
+        size_t nbytes = 0, nchars = 0, nlines = 0, nwords = 0;
 
-                for (size_t i = 0; i < nr; i++)
+        static char buffer[sizeof(wint_t)];
+        wint_t prevch = L' ', currch;
+        while ( (currch = fgetwc(fp)) != WEOF)
+        {
+                nchars++;
+                nbytes += wctomb(buffer, currch);
+                if (currch == L'\n')
                 {
-                        nbytes++;
-                        if (buffer[i] == '\n')
-                        {
-                                nlines++;
-                        }
-                        if (isspace(prevch) && !isspace(buffer[i]))
-                        {
-                                nwords++;
-                        }
-                        prevch = buffer[i];
+                        nlines++;
                 }
+                if (iswspace(prevch) && !iswspace(currch))
+                {
+                        nwords++;
+                }
+                prevch = currch;
+        }
+
+        if (fp != stdin)
+        {
+                fclose(fp);
         }
 
         if (modes & COUNT_LINES)
@@ -121,16 +114,28 @@ static void report_file(const char *file, int modes)
         {
                 printf("%zu ", nbytes);
         }
-        printf("%s\n", file);
-        close(fd);
+        if (modes & COUNT_CHARS)
+        {
+                printf("%zu ", nchars);
+        }
+        printf("%s\n", file == NULL ? "" : file);
 }
 
 int main(int argc, char *const argv[])
 {
+        setlocale(LC_CTYPE, "");
+
         struct input_args args = check_usage(argc, argv);
-        for (unsigned i = 0; args.files[i] != NULL; i++)
+        if (args.files[0] == NULL)
         {
-                report_file(args.files[i], args.modes);
+                report_file(NULL, args.modes);
+        }
+        else
+        {
+                for (unsigned i = 0; args.files[i] != NULL; i++)
+                {
+                        report_file(args.files[i], args.modes);
+                }
         }
 
         exit(exit_status);
