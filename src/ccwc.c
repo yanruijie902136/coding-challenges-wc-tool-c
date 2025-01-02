48448b945a1375c7776844a479bdabcbbd8b6033
yanruijie902136 <yanruijie902136@gmail.com>
2025-01-03 03:51:57 +0800

    Complete step 2

--- src/ccwc.c
+++ src/ccwc.c
@@ -7,20 +7,39 @@
 
 static int exit_status = EXIT_SUCCESS;
 
+enum count_mode
+{
+        COUNT_BYTES = 0x01,
+        COUNT_LINES = 0x02,
+};
+
+struct input_args
+{
+        char *const *files;
+        int modes;
+};
+
 static void print_usage_and_exit(void)
 {
-        fprintf(stderr, "usage: ccwc [-c] file ...\n");
+        fprintf(stderr, "usage: ccwc [-cl] file ...\n");
         exit(EXIT_FAILURE);
 }
 
-static char *const *check_usage(int argc, char *const argv[])
+static struct input_args check_usage(int argc, char *const argv[])
 {
+        struct input_args args;
+        args.modes = 0;
+
         int opt;
-        while ( (opt = getopt(argc, argv, ":c")) != -1)
+        while ( (opt = getopt(argc, argv, ":cl")) != -1)
         {
                 switch (opt)
                 {
                 case 'c':
+                        args.modes |= COUNT_BYTES;
+                        break;
+                case 'l':
+                        args.modes |= COUNT_LINES;
                         break;
                 case '?':
                         warnx("unrecognized option -- %c", optopt);
@@ -28,15 +47,20 @@ static char *const *check_usage(int argc, char *const argv[])
                 }
         }
 
-        char *const *paths = argv + optind;
-        if (paths[0] == NULL)
+        args.files = argv + optind;
+        if (args.files[0] == NULL)
         {
                 print_usage_and_exit();
         }
-        return paths;
+        if (args.modes == 0)
+        {
+                args.modes = COUNT_BYTES | COUNT_LINES;
+        }
+
+        return args;
 }
 
-static void report_file(const char *file)
+static void report_file(const char *file, int modes)
 {
         int fd = open(file, O_RDONLY);
         if (fd < 0)
@@ -47,7 +71,7 @@ static void report_file(const char *file)
         }
 
         static char buffer[BUFSIZ];
-        size_t nbytes = 0;
+        size_t nbytes = 0, nlines = 0;
         for ( ; ; )
         {
                 ssize_t nr = read(fd, buffer, sizeof(buffer));
@@ -60,20 +84,38 @@ static void report_file(const char *file)
                 }
                 else if (nr == 0)
                 {
-                        printf("%zu %s\n", nbytes, file);
-                        close(fd);
-                        return;
+                        break;
                 }
-                nbytes += nr;
+
+                for (size_t i = 0; i < nr; i++)
+                {
+                        nbytes++;
+                        if (buffer[i] == '\n')
+                        {
+                                nlines++;
+                        }
+                }
         }
+
+        if (modes & COUNT_LINES)
+        {
+                printf("%zu ", nlines);
+        }
+        if (modes & COUNT_BYTES)
+        {
+                printf("%zu ", nbytes);
+        }
+        printf("%s\n", file);
+        close(fd);
 }
 
 int main(int argc, char *const argv[])
 {
-        char *const *paths = check_usage(argc, argv);
-        for (unsigned i = 0; paths[i] != NULL; i++)
+        struct input_args args = check_usage(argc, argv);
+        for (unsigned i = 0; args.files[i] != NULL; i++)
         {
-                report_file(paths[i]);
+                report_file(args.files[i], args.modes);
         }
+
         exit(exit_status);
 }
